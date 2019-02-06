4b1b4927a2e8743a5dfb7dc0cf9f61b287eec1ce
Adelice <nancytoniw@gmail.com>
2019-02-06 11:24:04 +0200

    add strikethrough

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { UserFormComponent } from './user-form/user-form.component';
 import { UsersComponent } from './users/users.component';
 import {HttpClientModule} from '@angular/common/http';
-import {FormsModule} from '@angular/forms'
+import {FormsModule} from '@angular/forms';
+import { StrikethroughDirective } from './strikethrough.directive'
 @NgModule({
   declarations: [
     AppComponent,
    
     UserFormComponent,
-    UsersComponent
+    UsersComponent,
+    StrikethroughDirective
   ],
   imports: [
     BrowserModule,
